86c8f9cbc9b9958084247d9b97c8a77a73bc7ddb
Dennis Magno <dimagnoph@gmail.com>
2017-09-08 01:31:37 +0800

    Update

--- controllers/verification.js
+++ controllers/verification.js
@@ -2,11 +2,12 @@ module.exports = (req, res) => {
     const hubChallenge = req.query['hub.challenge'];
 
     const hubMode = req.query['hub.mode'];
-    const verifyTokenMatches = (req.query['hub.verify_token'] === 'botcube is cool');
+    const verifyTokenMatches = (req.query['hub.verify_token'] === 'EOL Aurora 2017');
 
     if (hubMode && verifyTokenMatches) {
         res.status(200).send(hubChallenge);
     } else {
+    	
         res.status(403).end();
     }
 };
