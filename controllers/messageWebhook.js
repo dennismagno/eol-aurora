2c5faf23393729eb6121769c97080c3f5aaed39d
Dennis Magno <dimagnoph@gmail.com>
2017-09-08 09:31:09 +0800

    Update

--- controllers/messageWebhook.js
+++ controllers/messageWebhook.js
@@ -3,11 +3,21 @@ const processMessage = require('../helpers/processMessage');
 module.exports = (req, res) => {
     if (req.body.object === 'page') {
         req.body.entry.forEach(entry => {
-            entry.messaging.forEach(event => {
-                if (event.message && event.message.text) {
-                    processMessage(event);
-                }
-            });
+            if(entry.hasOwnProperty('messaging')){
+                entry.messaging.forEach(event => {
+                    if (event.message && event.message.text) {
+                        processMessage(event);
+                    }
+                });
+            }
+
+            if(entry.hasOwnProperty('changes')){
+                entry.changes.forEach(event => {
+                    if(event.field=="feed" && event.value.item=="comment" && event.value.verb=="add") {
+                        
+                    }
+                });
+            }
         });
 
         res.status(200).end();
