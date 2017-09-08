c6abc5deb963a529fb67b84f3863a0388e4fba02
Dennis Icasiano Magno <magn340855@exactsoftware.com>
2017-09-08 13:25:59 +0800

    Generic

--- helpers/processMessage.js
+++ helpers/processMessage.js
@@ -34,6 +34,55 @@ const sendTextMessage = (senderId, text) => {
     });
 };
 
+const sendGenericMessage = (sender) => {
+    let messageData = {
+	    "attachment": {
+		    "type": "template",
+		    "payload": {
+				"template_type": "generic",
+			    "elements": [{
+					"title": "First card",
+				    "subtitle": "Element #1 of an hscroll",
+				    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+				    "buttons": [{
+					    "type": "web_url",
+					    "url": "https://www.messenger.com",
+					    "title": "web url"
+				    }, {
+					    "type": "postback",
+					    "title": "Postback",
+					    "payload": "Payload for first element in a generic bubble",
+				    }],
+			    }, {
+				    "title": "Second card",
+				    "subtitle": "Element #2 of an hscroll",
+				    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+				    "buttons": [{
+					    "type": "postback",
+					    "title": "Postback",
+					    "payload": "Payload for second element in a generic bubble",
+				    }],
+			    }]
+		    }
+	    }
+    }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:FACEBOOK_ACCESS_TOKEN},
+	    method: 'POST',
+	    json: {
+		    recipient: {id:sender},
+		    message: messageData,
+	    }
+    }, function(error, response, body) {
+	    if (error) {
+		    console.log('Error sending messages: ', error)
+	    } else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    });
+};
+
 module.exports = (event) => {
     const senderId = event.sender.id;
     const message = event.message.text;
@@ -47,6 +96,13 @@ module.exports = (event) => {
             sendImage(senderId, result);
         } else {
             console.log(event.sender);
+            if (event.message && event.message.text) {
+                let text = event.message.text
+                if (text === 'Generic') {
+                    sendGenericMessage(sender)
+                    continue
+                }
+            }
             sendTextMessage(senderId, result);
         }
     });
