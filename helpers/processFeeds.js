b10d786950b9d137c05c4387e8e482a30e4c4cbd
Dennis Icasiano Magno <magn340855@exactsoftware.com>
2017-09-14 00:04:38 +0800

    Test

--- helpers/processFeeds.js
+++ helpers/processFeeds.js
@@ -1,13 +1,14 @@
 const facebookAccessToken = {
     224239781409388 : 'EAABmovau6lkBAKGpc4uRCLBPlMLYLuGJZCJbUqPdDZAjSmSfgd35eASU4Blmyx9ehftKpC3XialyoGdtbHZBRHZAnuYpYqONGycgzUDbJs9AY1RRUT00KsBjnQXgBVWD6ZAgrkZBG0xRqzxiJRWBjMjZAFGEGLJlsAEmrebOFiVNVzvno3WBG7N8TBwt4CUU8ivJGWU93H0zAfe7Ym2Ip3B',
-    1617002078374787 : 'EAABmovau6lkBAEN32nDgs8rK05FW51XJFPdlstD4nSZBGHRZAedJfXMaykAV3dccGZArYXUAd7ljXunIzHx9Y20KWtLZAksub6laL9JZBq3lBCcZAyEsIpw8WX4pWzoXrnwlWxbszch5l9vEGOQZCaAEyZCNtKkRXgc23TKMq1ZCEMAZDZD'
-}
+    1617002078374787 : 'EAABmovau6lkBAEN32nDgs8rK05FW51XJFPdlstD4nSZBGHRZAedJfXMaykAV3dccGZArYXUAd7ljXunIzHx9Y20KWtLZAksub6laL9JZBq3lBCcZAyEsIpw8WX4pWzoXrnwlWxbszch5l9vEGOQZCaAEyZCNtKkRXgc23TKMq1ZCEMAZDZD',
+    486735555052779 : 'EAABmovau6lkBAN7EZCHAyZBhnZB0LS0pgiL55ZCDh9CPUWhDOVCDhsPMZBfauZCXEvhqtpp4lRYyPQoTysAqTznqZCuMcUKKTOB9tsl86eCpm7iybIPVuprg2MIJ6lmlB6ZBDjL5twPBvpUCN9Sm1T2J4d8TaP9kwUNEu43lZBw0LtQZDZD'
+};
 
 const facebookUserId = {
     852464038261499 : 1437288159680128,
     123939111678488 : 1540337582671286,
     1617002078374787: 1804321282941786
-}
+};
 
 //const FACEBOOK_ACCESS_TOKEN = 'EAABmovau6lkBAEN32nDgs8rK05FW51XJFPdlstD4nSZBGHRZAedJfXMaykAV3dccGZArYXUAd7ljXunIzHx9Y20KWtLZAksub6laL9JZBq3lBCcZAyEsIpw8WX4pWzoXrnwlWxbszch5l9vEGOQZCaAEyZCNtKkRXgc23TKMq1ZCEMAZDZD';
 //const FACEBOOK_ACCESS_TOKEN = 'EAABmovau6lkBAKGpc4uRCLBPlMLYLuGJZCJbUqPdDZAjSmSfgd35eASU4Blmyx9ehftKpC3XialyoGdtbHZBRHZAnuYpYqONGycgzUDbJs9AY1RRUT00KsBjnQXgBVWD6ZAgrkZBG0xRqzxiJRWBjMjZAFGEGLJlsAEmrebOFiVNVzvno3WBG7N8TBwt4CUU8ivJGWU93H0zAfe7Ym2Ip3B';
@@ -32,7 +33,7 @@ const sendQuickreply = (sender,pageid,message) => {
                                     ]
                         }
 
-    sendTemplateMessage(sender,messageData);
+    sendTemplateMessage(sender,pageid,messageData);
 };
 
 const sendTemplateMessage = (senderId,pageid, text) => {
@@ -59,7 +60,7 @@ const sendTextMessage = (senderId,pageid, text) => {
     });
 };
 
-const callPrivateReply = (messageData,comment_id) => {
+const callPrivateReply = (messageData,pageid,comment_id) => {
   request({
     uri: 'https://graph.facebook.com/v2.9/'+comment_id+'/Comments',
     qs: { access_token: facebookAccessToken[pageid]  },
@@ -75,10 +76,10 @@ const callPrivateReply = (messageData,comment_id) => {
 }
 
 module.exports = (event,type) => {
-    const commentId = event.value.comment_id;
-    const pageId = commentId.split("_")[0];
-    const senderId = facebookUserId[event.value.sender_id];
+    const commentId = event.value.post_id;
     const postId = event.value.post_id;
+    const pageId = postId.split("_")[0];
+    const senderId = facebookUserId[event.value.sender_id];
     const userMessage = event.value.message;
     var genericMessage = "";
     if (type == 0) {
