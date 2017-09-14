df26577b3f7ee11e08173a6442d182bec0e3c353
Dennis Icasiano Magno <magn340855@exactsoftware.com>
2017-09-14 11:08:00 +0800

    Sample

--- helpers/processFeeds.js
+++ helpers/processFeeds.js
@@ -87,7 +87,7 @@ module.exports = (event,type) => {
 
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
-        postMessage = response.body.message;
+        console.log(response.body);
     });
 
     var itemcode = '';
