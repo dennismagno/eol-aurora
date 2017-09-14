fe922c08cb88cc2c87f2e0fa674ec35a27f7f00e
Dennis Icasiano Magno <magn340855@exactsoftware.com>
2017-09-15 01:30:33 +0800

    Trigger

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const verificationController = require('./controllers/verification');
 const messageWebhookController = require('./controllers/messageWebhook');
 const imageSearchController = require('./controllers/imageSearch');
 
-var port = 5555;//process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 
 var path = require('path');
 
